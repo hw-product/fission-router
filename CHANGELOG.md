55951eafc88caafc5cc7230150dac993be12375d
Chris Roberts <chrisroberts.code@gmail.com>
2014-11-21 10:47:17 -0800

    Update files for release v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## v0.1.2
+* See commit log
+
 ## v0.1.0
 * Initial release
--- lib/fission-router/version.rb
+++ lib/fission-router/version.rb
@@ -1,7 +1,5 @@
 module Fission
   module Router
-    class Version < Gem::Version
-    end
-    VERSION = Version.new('0.1.1')
+    VERSION = Gem::Version.new('0.1.2')
   end
 end
