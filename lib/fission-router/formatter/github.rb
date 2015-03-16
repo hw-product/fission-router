8053fe4594e8627ff03f8e5793243a3bef42492b
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-16 09:04:55 -0700

    Add support to github formatter to restrict requested route application based on event type

--- lib/fission-router/formatter/github.rb
+++ lib/fission-router/formatter/github.rb
@@ -17,8 +17,24 @@ module Fission
         def format(payload)
           unless(payload.get(:data, :router, :requested_route))
             if(path = payload.get(:data, :github, :url_path))
-              payload.set(:data, :router, :requested_route, path.split('/').last)
+              route = path.split('/').last
+              unless_filtered(route, payload.get(:data, :github, :event) do
+                payload.set(:data, :router, :requested_route, route)
+              end
             end
+          end
+        end
+
+        # Check if event filtering is enabled for the requested path
+        # and do not call given block if event is filtered
+        #
+        # @param route [String] requested route name
+        # @param github_event [String] event name
+        # @return [Object] return value of block
+        def unless_filtered(route, github_event)
+          allowed_events = [config.fetch(:routes, route, :filters, :github, :events, [])].flatten.compact
+          if(allowed_events.empty? || allowed_events.include?(github_event))
+            yield if block_given?
           end
         end
 
